6f097bda912a649dca808f4d201e1b0c24ee2fe4
2955188397@qq.com <2955188397@qq.com>
2022-07-08 21:17:42 +0800

    第一次提交

---
+++ src/MyUpload.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react'
+import { UploadOutlined } from '@ant-design/icons';
+import {Upload,Button,UploadProps}from 'antd';
+function MyUpload() {
+  return (
+    <Upload>
+    <Button>
+      <UploadOutlined /> 点击上传网页
+    </Button>
+  </Upload>
+  )
+}
+
+export default MyUpload
