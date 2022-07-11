ff5ef7f9df8ce2bb928f9a3ca0224709592721f5
2955188397@qq.com <2955188397@qq.com>
2022-07-11 22:15:47 +0800

    feat:add table ande mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,45 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "antd";
+import {Table  } from 'antd';
 import "antd/dist/antd.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  //定义table列
+  const columns = [
+    {
+      title: '名称',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '地址',
+      dataIndex: 'addr',
+      key: 'addr',
+    },
+    {
+      title: '文件夹',
+      dataIndex: 'folder',
+      key: 'folder',
+    }
+  ];
+  // const dataSource=[
+  //   {key:"1",
+  //   name:"baidu",
+  //   addr:"www.baidu.com",
+  //   folder:"first"
+  //   }
+  // ]
+  var data={key:"1",
+  name:"baidu",
+  addr:"www.baidu.com",
+  folder:"first"
+  }
+  let dataSource=new Array(20).fill(data);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button>Button</Button>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+          <Table dataSource={dataSource} columns={columns}/>
     </div>
   );
 }
