48603bde8fe4ac33888faa92dedae5bbf5149aac
2955188397@qq.com <2955188397@qq.com>
2022-07-21 01:23:33 +0800

    feat:analyze html and display on the table

--- src/App.tsx
+++ src/App.tsx
@@ -21,17 +21,20 @@ function App() {
     },
     {
       title: '文件夹',
-      dataIndex: 'folder',
-      key: 'folder',
+      dataIndex: 'dirs',
+      key: 'dirs',
     },
   ];
   //模拟数据
-  const data = { key: '1', name: 'baidu', url: 'www.baidu.com', folder: 'first' };
+  const data = { key: '1', name: 'baidu', url: 'www.baidu.com', dirs: 'first' };
   //填充table表
-  const dataSource = new Array(1).fill(data);
+  //const dataSource = new Array(1).fill(data);
+  //
+  const [tableData, setTableData] = useState(Array);
+
   //  自定义上传函数
   const uploadHtml = (options: any) => {
-    const { onSuccess, onError, file, filename, onProgress } = options;
+    const { onSuccess, onError, file, filename, onProgress, onChange } = options;
     //将上传html网页转为字符串
     let reader = new FileReader();
     reader.readAsText(file);
@@ -45,24 +48,69 @@ function App() {
       //将字符串转为dom
       const parser = new DOMParser();
       const mainDocument = parser.parseFromString(htmlStr, 'text/html');
-      //获取title
-      console.log(mainDocument.title);
-      data.name = mainDocument.title;
-      //url(有问题)
-      console.log(mainDocument.URL);
-      data.url = mainDocument.URL;
-      //文件夹名（有问题）
-      //存入数组
-      Datalist.push(data);
+
+      //********************** */
+      const header = mainDocument.querySelector('h1');
+      //找到书签的根
+      const root = header.nextElementSibling;
+      //找到全部的书签
+      const linksElements = Array.from(root.querySelectorAll('a'));
+      let cnt = 0;
+      const links = linksElements.map((link) => {
+        let parent = link.parentNode;
+        const parents = [];
+
+        while (parent) {
+          //我们可以发现，所有的文件夹都是 H3, 然后加一个 DL
+          if (parent.tagName === 'DL') {
+            //找到 h3 的标题
+            parents.unshift(parent.previousSibling.previousSibling.textContent);
+          }
+          parent = parent.parentNode;
+        }
+        cnt++;
+        return {
+          key: cnt,
+          href: link.getAttribute('href'),
+          title: link.textContent,
+          //忽略最外面两层文件夹
+          dirs: parents.slice(2),
+        };
+      });
+      //console.log(links);
+      //*************************
+      //将数据写入table
+      let td = [];
+      td = change(links);
+      console.log(td);
+      setTableData(td.datas);
     };
-    console.log(Datalist);
+    //转换links中的数据，返回书签数据集合
+    let change = (links) => {
+      let datas = [];
+      //数据转换：将link数据转成tableData中元素
+      for (let value in links) {
+        //console.log(links[link]);
+        let data = {
+          key: links[value].key,
+          name: links[value].title,
+          url: links[value].href,
+          dirs: links[value].dirs,
+        };
+        datas.push(data);
+      }
+      return {
+        datas,
+      };
+    };
+    console.log(options);
   };
   return (
     <div className="App">
       <Upload customRequest={uploadHtml}>
         <Button icon={<UploadOutlined />}>Click to Upload</Button>
       </Upload>
-      <Table dataSource={dataSource} columns={columns} />
+      <Table dataSource={tableData} columns={columns} />
     </div>
   );
 }
